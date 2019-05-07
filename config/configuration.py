199f8eb2497ec170f753455dfda0f23f39966391
klapinsk <katarzyna.lapinska@intel.com>
2019-05-07 16:00:04 +0200

    Example test with config

---
+++ config/configuration.py
@@ -0,0 +1,11 @@
+#
+# Copyright(c) 2019 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+#
+
+
+opencas_repo_path = '/path/to/opencas/repo'
+test_wrapper_dir = '/path/to/test/wrapper'
+user = 'user'
+password = 'passwd'
+proxy_command = ''
