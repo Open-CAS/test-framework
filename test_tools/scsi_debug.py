227ee2e89348529478025ee1c21f5763fea3a03c
Robert Baldyga <robert.baldyga@unvertical.com>
2026-03-19 18:08:44 +0100

    scsi_debug: Move plugin into tool
    There is no reason for it to be a plugin. Make it a tool instead.
    
    Signed-off-by: Robert Baldyga <robert.baldyga@unvertical.com>

--- test_tools/scsi_debug.py
+++ test_tools/scsi_debug.py
@@ -1,85 +1,112 @@
 #
-# Copyright(c) 2022 Intel Corporation
+# Copyright(c) 2020-2022 Intel Corporation
 # Copyright(c) 2026 Unvertical
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
 import re
+from time import sleep
 
+from connection.utils.output import CmdException
 from core.test_run import TestRun
-from test_tools.os_tools import get_syslog_path
+from storage_devices.device import Device
+from test_tools.os_tools import (
+    get_syslog_path,
+    is_kernel_module_loaded,
+    load_kernel_module,
+    unload_kernel_module,
+)
+
+MODULE_NAME = "scsi_debug"
+
+FLUSH = re.compile(r"scsi_debug:[\s\S]*cmd 35")
+FUA = re.compile(r"scsi_debug:[\s\S]*cmd 2a 08")
 
 
-class Logs:
-    last_read_line = 0
-    syslog_path = None
-    FLUSH = re.compile(r"scsi_debug:[\s\S]*cmd 35")
-    FUA = re.compile(r"scsi_debug:[\s\S]*cmd 2a 08")
+class ScsiDebug:
+    def __init__(self, params):
+        self.params = params
+        self.syslog_path = None
+        self.last_read_line = 0
+        self.reload()
+
+    def reload(self):
+        self.unload()
+        sleep(1)
+        load_output = load_kernel_module(MODULE_NAME, self.params)
+        if load_output.exit_code != 0:
+            raise CmdException(f"Failed to load {MODULE_NAME} module", load_output)
+        TestRun.LOGGER.info(f"{MODULE_NAME} loaded successfully.")
+        sleep(10)
 
     @staticmethod
-    def mark():
+    def unload():
+        if is_kernel_module_loaded(MODULE_NAME):
+            unload_kernel_module(MODULE_NAME)
+
+    def get_devices(self):
+        scsi_debug_devices = TestRun.executor.run_expect_success(
+            "lsscsi --scsi_id | grep scsi_debug").stdout
+        return [Device(f'/dev/disk/by-id/scsi-{device.split()[-1]}')
+                for device in scsi_debug_devices.splitlines()]
+
+    def mark(self):
         """Set syslog position to current end so subsequent reads only see new entries."""
-        if Logs.syslog_path is None:
-            Logs.syslog_path = get_syslog_path()
+        if self.syslog_path is None:
+            self.syslog_path = get_syslog_path()
 
         line_count = TestRun.executor.run_expect_success(
-            f"wc -l < {Logs.syslog_path}"
+            f"wc -l < {self.syslog_path}"
         ).stdout.strip()
-        Logs.last_read_line = int(line_count)
+        self.last_read_line = int(line_count)
 
-    @staticmethod
-    def check_syslog_for_signals():
-        log_lines = Logs._read_syslog()
-        flush_count = Logs._count_logs(log_lines, Logs.FLUSH)
-        fua_count = Logs._count_logs(log_lines, Logs.FUA)
-        Logs._validate_logs_amount(fua_count, "FUA")
-        Logs._validate_logs_amount(flush_count, "FLUSH")
+    def check_for_signals(self):
+        log_lines = self._read_syslog()
+        flush_count, fua_count = self._count_logs(log_lines)
+        self._validate_logs_amount(fua_count, "FUA")
+        self._validate_logs_amount(flush_count, "FLUSH")
 
-    @staticmethod
-    def check_syslog_for_flush():
-        """Check syslog for FLUSH logs"""
-        log_lines = Logs._read_syslog()
-        flush_logs_counter = Logs._count_logs(log_lines, Logs.FLUSH)
-        Logs._validate_logs_amount(flush_logs_counter, "FLUSH")
+    def check_for_flush(self):
+        log_lines = self._read_syslog()
+        flush_count, _ = self._count_logs(log_lines)
+        self._validate_logs_amount(flush_count, "FLUSH")
 
-    @staticmethod
-    def check_syslog_for_fua():
-        """Check syslog for FUA logs"""
-        log_lines = Logs._read_syslog()
-        fua_logs_counter = Logs._count_logs(log_lines, Logs.FUA)
-        Logs._validate_logs_amount(fua_logs_counter, "FUA")
+    def check_for_fua(self):
+        log_lines = self._read_syslog()
+        _, fua_count = self._count_logs(log_lines)
+        self._validate_logs_amount(fua_count, "FUA")
 
-    @staticmethod
-    def _read_syslog():
+    def _read_syslog(self):
         """Read syslog lines since last mark and advance the position."""
-        if Logs.syslog_path is None:
-            Logs.syslog_path = get_syslog_path()
+        if self.syslog_path is None:
+            self.syslog_path = get_syslog_path()
 
-        start_line = Logs.last_read_line + 1
+        start_line = self.last_read_line + 1
         log_lines = TestRun.executor.run_expect_success(
-            f"tail -qn +{start_line} {Logs.syslog_path}"
+            f"tail -qn +{start_line} {self.syslog_path}"
         ).stdout.splitlines()
-        Logs.last_read_line += len(log_lines)
+        self.last_read_line += len(log_lines)
         return log_lines
 
     @staticmethod
-    def _count_logs(log_lines: list, expected_log):
-        """Count specified log in list and return its amount."""
-        logs_counter = 0
+    def _count_logs(log_lines):
+        flush_count = 0
+        fua_count = 0
         for line in log_lines:
-            if expected_log.search(line) is not None:
-                logs_counter += 1
-        return logs_counter
+            if FLUSH.search(line):
+                flush_count += 1
+            if FUA.search(line):
+                fua_count += 1
+        return flush_count, fua_count
 
     @staticmethod
-    def _validate_logs_amount(logs_counter: int, log_type: str):
-        """Validate amount of logs."""
-        if logs_counter == 0:
+    def _validate_logs_amount(count, log_type):
+        if count == 0:
             if log_type == "FLUSH":
                 TestRun.LOGGER.error(f"{log_type} log not occured")
             else:
                 TestRun.LOGGER.warning(f"{log_type} log not occured")
-        elif logs_counter == 1:
+        elif count == 1:
             TestRun.LOGGER.warning(f"{log_type} log occured only once.")
         else:
-            TestRun.LOGGER.info(f"{log_type} log occured {logs_counter} times.")
+            TestRun.LOGGER.info(f"{log_type} log occured {count} times.")
