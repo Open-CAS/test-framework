cd1f0b8dae43704c19a4610ba1858cf677992293
Robert Baldyga <robert.baldyga@unvertical.com>
2026-03-19 16:45:42 +0100

    scsi_debug: Handle syslog parsing properly
    Signed-off-by: Robert Baldyga <robert.baldyga@unvertical.com>

--- test_tools/scsi_debug.py
+++ test_tools/scsi_debug.py
@@ -1,69 +1,81 @@
 #
 # Copyright(c) 2022 Intel Corporation
+# Copyright(c) 2026 Unvertical
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
 import re
 
 from core.test_run import TestRun
-
-syslog_path = "/var/log/messages"
+from test_tools.os_tools import get_syslog_path
 
 
 class Logs:
-    last_read_line = 1
+    last_read_line = 0
+    syslog_path = None
     FLUSH = re.compile(r"scsi_debug:[\s\S]*cmd 35")
     FUA = re.compile(r"scsi_debug:[\s\S]*cmd 2a 08")
 
     @staticmethod
+    def mark():
+        """Set syslog position to current end so subsequent reads only see new entries."""
+        if Logs.syslog_path is None:
+            Logs.syslog_path = get_syslog_path()
+
+        line_count = TestRun.executor.run_expect_success(
+            f"wc -l < {Logs.syslog_path}"
+        ).stdout.strip()
+        Logs.last_read_line = int(line_count)
+
+    @staticmethod
     def check_syslog_for_signals():
-        Logs.check_syslog_for_flush()
-        Logs.check_syslog_for_fua()
+        log_lines = Logs._read_syslog()
+        flush_count = Logs._count_logs(log_lines, Logs.FLUSH)
+        fua_count = Logs._count_logs(log_lines, Logs.FUA)
+        Logs._validate_logs_amount(fua_count, "FUA")
+        Logs._validate_logs_amount(flush_count, "FLUSH")
 
     @staticmethod
     def check_syslog_for_flush():
         """Check syslog for FLUSH logs"""
-        log_lines = Logs._read_syslog(Logs.last_read_line)
+        log_lines = Logs._read_syslog()
         flush_logs_counter = Logs._count_logs(log_lines, Logs.FLUSH)
-        log_type = "FLUSH"
-        Logs._validate_logs_amount(flush_logs_counter, log_type)
+        Logs._validate_logs_amount(flush_logs_counter, "FLUSH")
 
     @staticmethod
     def check_syslog_for_fua():
         """Check syslog for FUA logs"""
-        log_lines = Logs._read_syslog(Logs.last_read_line)
+        log_lines = Logs._read_syslog()
         fua_logs_counter = Logs._count_logs(log_lines, Logs.FUA)
-        log_type = "FUA"
-        Logs._validate_logs_amount(fua_logs_counter, log_type)
+        Logs._validate_logs_amount(fua_logs_counter, "FUA")
 
     @staticmethod
-    def _read_syslog(last_read_line: int):
-        """Read recent lines in syslog, mark last line and return read lines as list."""
-        log_lines = TestRun.executor.run_expect_success(
-            f"tail -qn +{last_read_line} {syslog_path}"
-        ).stdout.splitlines()
-        # mark last read line to continue next reading from here
-        Logs.last_read_line += len(log_lines)
+    def _read_syslog():
+        """Read syslog lines since last mark and advance the position."""
+        if Logs.syslog_path is None:
+            Logs.syslog_path = get_syslog_path()
 
+        start_line = Logs.last_read_line + 1
+        log_lines = TestRun.executor.run_expect_success(
+            f"tail -qn +{start_line} {Logs.syslog_path}"
+        ).stdout.splitlines()
+        Logs.last_read_line += len(log_lines)
         return log_lines
 
     @staticmethod
     def _count_logs(log_lines: list, expected_log):
         """Count specified log in list and return its amount."""
         logs_counter = 0
-
         for line in log_lines:
-            is_log_in_line = expected_log.search(line)
-            if is_log_in_line is not None:
+            if expected_log.search(line) is not None:
                 logs_counter += 1
-
         return logs_counter
 
     @staticmethod
     def _validate_logs_amount(logs_counter: int, log_type: str):
-        """Validate amount of logs and return"""
+        """Validate amount of logs."""
         if logs_counter == 0:
-            if Logs._is_flush(log_type):
+            if log_type == "FLUSH":
                 TestRun.LOGGER.error(f"{log_type} log not occured")
             else:
                 TestRun.LOGGER.warning(f"{log_type} log not occured")
@@ -71,7 +83,3 @@ class Logs:
             TestRun.LOGGER.warning(f"{log_type} log occured only once.")
         else:
             TestRun.LOGGER.info(f"{log_type} log occured {logs_counter} times.")
-
-    @staticmethod
-    def _is_flush(log_type: str):
-        return log_type == "FLUSH"
